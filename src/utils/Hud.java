f56b535fc5856726942bea3227ada3c632666104
Vingadassamy Léo <Lo@Lo-PC>
2017-01-11 19:32:31 +0100

    Ajout nouvelles ressources + organisation

--- src/utils/Hud.java
+++ src/utils/Hud.java
@@ -14,34 +14,40 @@ public class Hud {
 	  private static final int BAR_WIDTH = 50;
 	  private static final int BAR_HEIGHT = 10;
 
-	  private static final Color LIFE_COLOR = new Color(255, 0, 0);
+	  private static final Color LIFE_COLOR = new Color(208, 46, 73);
 	  private static final Color STRING_COLOR = new Color(255, 255, 255);
 
-	  
+	  private Image lightSpell;
 	  private Image spellFire;
-	  private Image spellFrost;
-	  private Image contour;
+	  private Image frostArrow;
+	  private Image circleArrow;
 
 	  public Hud() throws SlickException 
 	  {
 		  this.init();
 	  }
+	  
 	  public void init() throws SlickException {
-	    this.playerBars = new Image("ressources/player-bar.png");
-	    this.spellFire = new Image("ressources/spellFire.png");
-	    this.spellFrost = new Image("ressources/spellFrost.png");
-	    this.contour = new Image("ressources/contour.png");
+	    this.playerBars = new Image("ressources/HUD/rock_bar.png");
+	    this.spellFire = new Image("ressources/HUD/spellFire.png");
+	    this.frostArrow = new Image("ressources/HUD/frostArrow.png");
+	    this.lightSpell = new Image("ressources/HUD/lightSpell.png");
+
+	    this.circleArrow = new Image("ressources/HUD/circleArrowSpell.png");
 	  }
 	  
 	  
 	  public void render(GameContainer container, Graphics g, ArrayList<Player> player, Player p) {
 		  g.setColor(LIFE_COLOR);
-		  g.fillRect(p.x - 23, p.y - 55, (float) ((p.getLife()*0.01) * BAR_WIDTH), BAR_HEIGHT);
+		  g.drawImage(playerBars, p.x -23, p.y-55);
+
+		  g.fillRect(p.x - 23, p.y - 53, (float) ((p.getLife()*0.01) * BAR_WIDTH), BAR_HEIGHT);
 		  g.setColor(STRING_COLOR);
 		  g.drawString(""+p.getLife(),p.x - 15, p.y - 60);
 		  for (int i = 0; i < player.size(); i++)
 		  {
 			  g.setColor(LIFE_COLOR);
+			  g.drawImage(playerBars, player.get(i).x -23, player.get(i).y-55);
 			  g.fillRect(player.get(i).x - 23, player.get(i).y - 55, (float) ((player.get(i).getLife()*0.01) * BAR_WIDTH), BAR_HEIGHT);
 			  g.setColor(STRING_COLOR);
 			  g.drawString(""+player.get(i).getLife(),player.get(i).x - 15, player.get(i).y - 60);
@@ -49,8 +55,8 @@ public class Hud {
 		  
 		  
 		  	g.drawImage(spellFire, p.getX()-50 ,p.getY() + 326);
-		  	g.drawImage(spellFrost, p.getX() + 56 - 50, p.getY() + 326);
-		  	g.drawImage(contour, p.getX() + 56*2 - 50, p.getY() + 326);
-		  	g.drawImage(contour, p.getX() + 56*3 - 50, p.getY() + 326);
+		  	g.drawImage(frostArrow, p.getX() + 56 - 50, p.getY() + 326);
+		  	g.drawImage(lightSpell, p.getX() + 56*2 - 50, p.getY() + 326);
+		  	g.drawImage(circleArrow, p.getX() + 56*3 - 50, p.getY() + 326);
 		}
 }
--- src/utils/Spells.java
+++ src/utils/Spells.java
@@ -7,6 +7,8 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.svg.InkscapeLoader;
+import org.newdawn.slick.svg.SimpleDiagramRenderer;
 
 import utils.Attack;
 
@@ -14,33 +16,33 @@ public class Spells {
 	public ArrayList<Attack> allSpell = new ArrayList<>();
 	private ArrayList<Animation> fire = new ArrayList<>();
 	
+	
 /**
  * Loading the sprites
  * @throws SlickException
  */
 	public Spells() throws SlickException {
 		
-		
 		Animation u = new Animation();
-		SpriteSheet sU = new SpriteSheet("ressources/fireballUp.png", 31, 69);
+		SpriteSheet sU = new SpriteSheet("ressources/fireBall/fireballUp.png", 31, 69);
 		u.addFrame(sU.getSprite(0, 0), 100);
 		fire.add(u);
 		
 		
 		
 		Animation l = new Animation();
-		SpriteSheet sL = new SpriteSheet("ressources/fireballLeft.png", 69, 31);
+		SpriteSheet sL = new SpriteSheet("ressources/circleArrow/circleArrowL.png", 100, 33);
 		l.addFrame(sL.getSprite(0, 0), 100);
 		fire.add(l);
 		
 		
-		SpriteSheet sD = new SpriteSheet("ressources/fireballDown.png", 31, 69);	
+		SpriteSheet sD = new SpriteSheet("ressources/spells/shadowBall.png", 31, 69);	
 		Animation d = new Animation();
 		d.addFrame(sD.getSprite(0, 0), 100);
 		fire.add(d);
 
 		
-		SpriteSheet sR = new SpriteSheet("ressources/fireball.png", 69, 31);
+		SpriteSheet sR = new SpriteSheet("ressources/pinkArrow/pinkArrowR.png", 69, 31);
 		Animation r = new Animation();
 		r.addFrame(sR.getSprite(0, 0), 100);
 		fire.add(r);
@@ -51,8 +53,10 @@ public class Spells {
 		if (allSpell.size() > 0){
 			for (int i = 0 ; i < allSpell.size(); i++) {
 				Attack att = this.allSpell.get(i);
+				if (att.getName().equals("fireball"))
+				{
 				g.drawAnimation(fire.get(att.getDirection()), att.getXbeg(),  att.getYbeg());
-				if ((allSpell.get(i).getXbeg() == allSpell.get(i).getXbeg()) || (allSpell.get(i).getYbeg() == allSpell.get(i).getYbeg())){
+				if ((allSpell.get(i).getXbeg() == allSpell.get(i).getXbeg()) || (allSpell.get(i).getYbeg() == allSpell.get(i).getYbeg()))
 					this.allSpell.remove(allSpell.get(i));
 				}
 			}
